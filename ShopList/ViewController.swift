0fba0dab3dbb186a7a6ed3ddcab22223ae2d2e05
usamaeltmsah <usamaeltmsah@gmail.com>
2020-11-02 22:54:51 +0200

    Make a table view with 3 rows all contains "Hello World"

--- ShopList/ViewController.swift
+++ ShopList/ViewController.swift
@@ -7,11 +7,25 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell: UITableViewCell = self.tableView.dequeueReusableCell(withIdentifier: "cell")!
+        
+        cell.textLabel!.text = "Hello World"
+        
+        return cell
+    }
+    
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        self.tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
     }
 
 
