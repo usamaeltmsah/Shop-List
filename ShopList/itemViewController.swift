8215e093173ffefcda1f839c1fa5655c31331165
usamaeltmsah <usamaeltmsah@gmail.com>
2020-11-02 23:50:09 +0200

    Design an Expanded View

---
+++ ShopList/itemViewController.swift
@@ -0,0 +1,18 @@
+//
+//  itemViewController.swift
+//  ShopList
+//
+//  Created by Usama Eltmsah on 11/2/20.
+//
+
+import UIKit
+
+class itemViewController: UIViewController {
+
+    @IBOutlet weak var itemName: UILabel!
+    @IBOutlet weak var itemCount: UILabel!
+    @IBOutlet weak var itemImage: UIImageView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
